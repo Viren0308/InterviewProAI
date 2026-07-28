6afe0dcea86ba52af01bc121866a5c608e753753
viren1924 <virushelavale1924@gmail.com>
2026-07-28 15:38:06 +0530

    feat(auth): add user model and alembic migration

--- alembic/env.py
+++ alembic/env.py
@@ -1,5 +1,6 @@
 from logging.config import fileConfig
-
+from app.core.database import Base
+from app.models.user import User
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
@@ -18,7 +19,8 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
